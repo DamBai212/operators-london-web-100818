6502391b0cfb0113e54c8007ab8c46c057a45384
DamBai212 <damanybailey@icloud.com>
2018-10-07 16:00:41 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,10 +3,7 @@ def unsafe?(speed)
     return true
   else
     return false
- end
-end
-
- def not_safe?(speed)
+end	end
+ def not_safe?(speed)	def not_safe?(speed)
 		  speed < 40 && speed > 60 ? true : false
- end
-en
+end	end
